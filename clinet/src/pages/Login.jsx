79b1cc069749b83991c039ac0a790509ff4c754a
just-code001 <156091121+just-code001@users.noreply.github.com>
2024-03-07 07:26:20 +0530

    validation add in login and signup form

--- clinet/src/pages/Login.jsx
+++ clinet/src/pages/Login.jsx
@@ -12,33 +12,52 @@ import {
 // import * as yup from "yup";
 import CameraAltIcon from "@mui/icons-material/CameraAlt";
 import { VisuallyHidden } from "../Components/styles/StyledComponent";
-
-// const schema=yup.object({
-//   email:yup.string().email(),
-//   password:yup.string().min(3).Required("Email Id is required")
-// })
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
+
+  const schema = yup.object({
+    email: yup.string().email().required(),
+    password: yup.string().min(3).required("Password  is required"),
+  });
+  // if (!isLogin) {
+  //   schema.name = yup.string().required("name is required");
+  //   schema.confirmPassword = yup
+  //     .string()
+  //     .required("Confirm Password is required");
+  // }
+
+  // const schema = yup.object(validationShape);
+
+  const init = {
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const {
+    register,
+    handleSubmit,
+    // setValue,
+    // watch,
+    formState: { errors },
+  } = useForm({
+    defaultVales: init,
+    resolver: yupResolver(schema),
+  });
 
   const toggleLogin = () => {
     setIsLogin((prev) => !prev);
   };
 
-  const init={
-    name:"",email:"", password:"", confirmPassword:""
-  }
-  const [formData,setFormData] = useState(init)
-
-  const handleChange = (e) => {
-    const {value}=e.target
-    setFormData((prev)=>{
-      return{
-        ...prev,
-        [e.target.value]:value
-      }
-    })
-  }
+  const onSubmit = (data, e) => {
+    console.log(data, "llllll", e);
+  };
+  console.log(errors, "error");
   return (
     <Container
       maxWidth="xs"
@@ -63,30 +82,53 @@ const Login = () => {
         {isLogin ? (
           <>
             <Typography variant="h5">Login</Typography>
-            <form style={{ width: "100%" }} onSubmit={() => {}}>
+            <form style={{ width: "100%" }} onSubmit={handleSubmit(onSubmit)}>
               <TextField
+                required
                 fullWidth
                 label="Email"
                 margin="normal"
                 name="email"
                 variant="outlined"
-                value={formData.name}
-                onChange={handleChange}
+                {...register("email")}
               />
+              {errors?.email?.message && (
+                <span
+                  style={{
+                    color: "#f14040",
+                    fontSize: "0.8rem",
+                    marginTop: "0px",
+                  }}
+                >
+                  {errors?.email?.message}
+                </span>
+              )}
               <TextField
+                required
                 type="password"
                 fullWidth
                 label="Password"
                 margin="normal"
                 name="password"
                 variant="outlined"
-                value={formData.password}
+                {...register("password")}
               />
+              {errors?.password?.message && (
+                <span
+                  style={{
+                    color: "#f14040",
+                    fontSize: "0.8rem",
+                    marginTop: "0px",
+                  }}
+                >
+                  {errors?.password?.message}
+                </span>
+              )}
               <Button
                 variant="contained"
                 color="primary"
                 type="submit"
-                fullWidth="true"
+                fullWidth
                 sx={{ marginTop: "1rem" }}
               >
                 Login
@@ -100,7 +142,7 @@ const Login = () => {
               <Button
                 variant="text"
                 color="primary"
-                fullWidth="true"
+                fullWidth
                 type="submit"
                 onClick={() => {
                   toggleLogin();
@@ -112,8 +154,8 @@ const Login = () => {
           </>
         ) : (
           <>
-            <h2>sign up</h2>
-            <form style={{ width: "100%" }} onSubmit={() => {}}>
+            <Typography variant="h5" className="mb-3">Sign up</Typography>
+            <form style={{ width: "100%" }} onSubmit={handleSubmit(onSubmit)}>
               <Stack position={"relative"} width={"10rem"} margin={"auto"}>
                 <Avatar
                   sx={{
@@ -122,57 +164,103 @@ const Login = () => {
                     objectFit: "contain",
                   }}
                 />
-                <IconButton
-                  sx={{ position: "absolute", right: 0, bottom: 0 }}
-                >
+                <IconButton sx={{ position: "absolute", right: 0, bottom: 0 }}>
                   <>
-                  <label htmlFor="file-input">
-                    <CameraAltIcon />
-                    <VisuallyHidden type="file" id="file-input"/>
-                  </label>
+                    <label htmlFor="file-input">
+                      <CameraAltIcon />
+                      <VisuallyHidden type="file" id="file-input" />
+                    </label>
                   </>
                 </IconButton>
               </Stack>
               <TextField
                 required
-                name="userName"
+                name="name"
                 label="Name"
                 margin="normal"
                 variant="outlined"
                 fullWidth
+                {...register("name")}
               />
+              {errors?.name?.message && (
+                <span
+                  style={{
+                    color: "#f14040",
+                    fontSize: "0.8rem",
+                    marginTop: "0px",
+                  }}
+                >
+                  {errors?.name?.message}
+                </span>
+              )}
               <TextField
                 required
-                name="userEmail"
+                name="email"
                 label="Email"
                 margin="normal"
                 variant="outlined"
                 fullWidth
+                {...register("email")}
               />
+              {errors?.email?.message && (
+                <span
+                  style={{
+                    color: "#f14040",
+                    fontSize: "0.8rem",
+                    marginTop: "0px",
+                  }}
+                >
+                  {errors?.email?.message}
+                </span>
+              )}
               <TextField
                 required
+                type="password"
                 name="password"
                 label="Password"
                 margin="normal"
                 variant="outlined"
                 fullWidth
+                {...register("password")}
               />
+              {errors?.password?.message && (
+                <span
+                  style={{
+                    color: "#f14040",
+                    fontSize: "0.8rem",
+                    marginTop: "0px",
+                  }}
+                >
+                  {errors?.password?.message}
+                </span>
+              )}
               <TextField
                 required
+                type="password"
                 name="ConfirmPassword"
                 label="Confirm Password"
                 margin="normal"
                 variant="outlined"
                 fullWidth
+                {...register("ConfirmPassword")}
               />
-
+              {errors?.ConfirmPassword?.message && (
+                <span
+                  style={{
+                    color: "#f14040",
+                    fontSize: "0.8rem",
+                    marginTop: "0px",
+                  }}
+                >
+                  {errors?.ConfirmPassword?.message}
+                </span>
+              )}
               <Button
                 className="mt-1"
                 variant="contained"
                 color="primary"
                 fullWidth
                 type="submit"
-                onClick={() => {}}
               >
                 Submit
               </Button>
